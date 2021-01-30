2ee2fb064e011f8f0b449380b67287f5bb5f5a2b
k.Adam <kloczl.adam@gmail.com>
2021-01-30 23:21:54 +0100

    updated ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Grid from '@material-ui/core/Grid';
 import * as Constants from '../utils/Constants'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 import axios from 'axios'
 
 function Layout() {
@@ -28,9 +29,18 @@ function Layout() {
         <div className="layout">
             <CircularProgress style={{display: showSpinner ? 'block' : 'none' }} className="spinner"/>
 
+            <h2>Search for a movie</h2>
+            
+            <TextField id="standard-basic" placeholder="Interstellar" label="Enter movie title here" size="medium"/>
+
+            <h1>Trending movies</h1>
+
             <Grid className="grid" container spacing={4} alignItems="center" justify="center">
                 {data.map(movie => (
-                    <Grid className="grid__item" item xs={12} sm={6} md={4} lg={3}>
+                    <Grid className="grid__item" direction="column" item xs={12} sm={6} md={4} lg={3}>
+                        <div className="grid__title">
+                            <h3>{movie.name}</h3>
+                        </div>
                         <img className="grid__img" src={movie.img.url} alt={movie.name}/>
                     </Grid>
                 ))}
