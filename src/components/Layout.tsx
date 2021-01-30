c0fcdae112f5d00ef9639bfb2599eb1ae41c7b9d
k.Adam <kloczl.adam@gmail.com>
2021-01-31 00:31:02 +0100

    search bar added with ui changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import * as Constants from '../utils/Constants'
 import CircularProgress from '@material-ui/core/CircularProgress';
-import TextField from '@material-ui/core/TextField';
+import SearchBar from "material-ui-search-bar";
 import Grid from '@material-ui/core/Grid';
 import axios from 'axios'
 
@@ -10,34 +10,47 @@ function Layout() {
     const [showSpinner, setShowSpinner] = useState(true);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const queryResult = await axios.post(
+        fetchData(false);
+    }, []);
+
+    const fetchData = async (isSearch : boolean) => {
+        let queryResult:any;
+        if(isSearch){
+            queryResult = await axios.post(
+                Constants.GRAPHQL_API, {
+                    query: Constants.FETCH_SEARCH_QUERY
+                }
+            );
+            setShowSpinner(true)
+            console.log(data)
+        }
+        else{
+            queryResult = await axios.post(
                 Constants.GRAPHQL_API, {
                     query: Constants.FETCH_POPULAR_QUERY
                 }
             );
-            const result = queryResult.data.data;
-            setData(result.movies)
             setShowSpinner(false);
-            console.log(data)
         }
-
-        fetchData();
-    }, []);
+        const result = queryResult.data.data;
+        setData(result.movies)
+        console.log(data)
+    }
     
     return (
         <div className="layout">
             <CircularProgress style={{display: showSpinner ? 'block' : 'none' }} className="spinner"/>
 
             <h2>Search for a movie</h2>
-            
-            <TextField id="standard-basic" placeholder="Interstellar" label="Enter movie title here" size="medium"/>
+
+            <SearchBar className="searchBar" onRequestSearch={() => fetchData(true)} placeholder="Interstellar" cancelOnEscape
+            />
 
             <h1>Trending movies</h1>
 
             <Grid className="grid" container spacing={4} alignItems="center" justify="center">
                 {data.map(movie => (
-                    <Grid className="grid__item" direction="column" item xs={12} sm={6} md={4} lg={3}>
+                    <Grid className="grid__item" item xs={12} sm={6} md={4} lg={3}>
                         <div className="grid__title">
                             <h3>{movie.name}</h3>
                         </div>
