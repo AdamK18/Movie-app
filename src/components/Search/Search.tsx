a6d3a5ea306600db9485ccf622af41e7bf72707e
k.Adam <kloczl.adam@gmail.com>
2021-02-05 11:15:16 +0100

    edited header

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 
 import SearchBar from 'material-ui-search-bar';
-import ReplayIcon from '@material-ui/icons/Replay';
-import Button from '@material-ui/core/Button';
 
 import './search.css';
 
@@ -12,32 +10,20 @@ const Search = ({ updateMovies }: any) => {
 	const [userInput, setUserInput] = useState('');
 
 	return (
-		<div className="header__search-container">
-			<Button
-				className="header__reload-btn"
-				onClick={() => {
-					setUserInput('');
-					updateMovies(operation.TRENDING, '');
-				}}
-			>
-				<ReplayIcon />
-			</Button>
-
-			<SearchBar
-				value={userInput}
-				onChange={(value) => setUserInput(value)}
-				className="searchBar"
-				placeholder="Interstellar"
-				cancelOnEscape
-				onRequestSearch={() =>
-					updateMovies(
-						userInput === '' ? operation.TRENDING : operation.SEARCH,
-						userInput
-					)
-				}
-				onCancelSearch={() => updateMovies(operation.TRENDING, '')}
-			/>
-		</div>
+		<SearchBar
+			value={userInput}
+			onChange={(value) => setUserInput(value)}
+			className="searchBar"
+			placeholder="Interstellar"
+			cancelOnEscape
+			onRequestSearch={() =>
+				updateMovies(
+					userInput === '' ? operation.TRENDING : operation.SEARCH,
+					userInput
+				)
+			}
+			onCancelSearch={() => updateMovies(operation.TRENDING, '')}
+		/>
 	);
 };
 
